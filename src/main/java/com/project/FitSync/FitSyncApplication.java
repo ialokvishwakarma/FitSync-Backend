4b77f67ab63850ac194eeb7b9f3d8d5942753e63
ialokvishwakarma <av2632301@gmail.com>
2026-05-06 19:24:49 +0530

    Initial Commit - FitSync

---
+++ src/main/java/com/project/FitSync/FitSyncApplication.java
@@ -0,0 +1,14 @@
+package com.project.FitSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitSyncApplication.class, args);
+		System.out.println("Hello Hell");
+	}
+
+}
---
+++ src/test/java/com/project/FitSync/FitSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FitSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
